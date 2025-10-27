2e4c1e0ea49a7876e20ca1f9bf973e647cc2567c
pkc <906847109@qq.com>
2025-10-27 20:51:17 +0800

    数据库初始化

--- Program.cs
+++ Program.cs
@@ -26,11 +26,21 @@ namespace LyricsWeb
             {
                 app.MapOpenApi();
             }
-
             app.UseHttpsRedirection();
-
             app.UseAuthorization();
 
+            using (var scope = app.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<ApplicationDbContext>();
+                    context.Database.Migrate();
+                }
+                catch 
+                {
+                }
+            }
 
             app.MapControllers();
 
